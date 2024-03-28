1fcea16cd2c39cc01d83d4c04b543c58eea17ed2
WhatsApp <75611985+YEWhatsApp@users.noreply.github.com>
2024-03-28 06:00:27 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,28 +1,161 @@
-const express=require("express");
-const mongo = require("mongoose");
-const post = require("./modules/post");
-const app = express();
-//const mongo = require("mongoose");
-mongo.connect("mongodb+srv://ziad:ziad2321@nodejsproject.qb18fmw.mongodb.net/?retryWrites=true&w=majority").then(() =>{console.log("sucess")}).catch((error) => {console.log("error",error)});
-app.get("/hi",(req,res) => {
-res.send("hello");
+const express = require("express");
+//const fun =require(__dirname + "/up2.ejs");
+//const firestor = require("firebase/app");
+//console.log(firestor.ref);
+//import "firebase/storage";
+const { initializeApp } = require('firebase/app');
+const {
+  getStorage,
+  ref,
+  uploadBytes,
+  uploadBytesResumable,
+  getDownloadURL,
+} = require('firebase/storage');
+const multer = require('multer');
+
+const app =express();
+app.use((req, res, next) => {
+  res.status(404).send('status');
 });
-//function getZi (str){return str.length}
-app.get("/getPost",async (req,res) => {
-const postzi = await post.find();
-res.json(postzi);
+const firebaseConfig = {
+  apiKey: "AIzaSyCp6A7rkCe1HwZOxCd2DkV1uOZBjabGkuM",
+  authDomain: "zinode-8e2a4.firebaseapp.com",
+  projectId: "zinode-8e2a4",
+  storageBucket: "zinode-8e2a4.appspot.com",
+  messagingSenderId: "756060139431",
+  appId: "1:756060139431:web:49c7f1d0afb2b0c8085082",
+  databaseURL:"https://zinode-8e2a4-default-rtdb.asia-southeast1.firebasedatabase.app"
+}; 
+
+const firebaseApp = initializeApp(firebaseConfig);
+const firestore = getStorage(firebaseApp);
+const request = require('superagent')
+// Multer setup
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+function progress_middleware(req, res, next){
+    let progress = 0;
+    const file_size = req.headers["content-length"];
+    
+    // set event listener
+    req.on("data", (chunk) => {
+        progress += chunk.length;
+        const percentage = (progress / file_size) * 100;
+        // other code ...
+    });
+
+    // invoke next middleware
+    next();
+}
+
+var s = "heloo ";
+const func = require("./fun");
+let i  ={get(){return s ;}};
+//console.log(i );
+var si=15 ;
+//const db = require("firebase-admin/database");
+//const dbd = db.getDatabase();
+//const re =db.child('user');
+// re.set({name:"ali"});
+console.log(firebaseApp.getDatabase());
+app.get('/',(req,res)=>{
+//const imageURL = await getDownloadURL(snapshot.ref);
+//    var pathReference =ref('folder.jpg')
+//console.log(pathReference)
+//var url = getDownloadURL(pathReference);
+var url ="https://firebasestorage.googleapis.com/v0/b/zinode-8e2a4.appspot.com/o/products%2F1711235971174-folder.jpg?alt=media&token=030c41c4-5ade-47c9-b75e-612187336b8d"
+/*
+var xhr = new XMLHttpRequest();
+      xhr.responseType = 'blob';
+      xhr.onload = (event) => {
+        var blob = xhr.response;
+
+      };
+      xhr.open('GET', url);
+      xhr.send();
+
+*/ 
+
+res.render(__dirname + "/up2.ejs",{size:si,fun : func,i : i  });
+//res.write(`<div>zoufuf </div>`)
+
+/*
+res.set(
+     'Content-Disposition',
+     'attachment; filename=some_file_name.png'
+   );
+request(url).pipe(res);*/
+//res.redirect('/')
 });
-app.get("/post",async (req,res) => {
-const newPost = new post();
-newPost.header="Header";
-newPost.body="BODY";
-newPost.footer=200;
-await newPost.save();
-res.send(`post saved`);
+app.get('/:id',(req,res)=>{
+console.log(req.params)
 });
-app.get("/index",(req,res) => {
-res.render("index.ejs",{name : ["Ali","ahemd","Ziad"]});
+
+app.post('/',upload.single('image'), async (req, res, next) => {
+  const file = req.file;
+//res.redirect('/');
+let time =new Date();
+//console.log(res)
+//res.write (`<div>${time}</div>`)
+/*
+var n ="ggfffc.jpg"
+var url ="https://firebasestorage.googleapis.com/v0/b/zinode-8e2a4.appspot.com/o/products%2F1711235971174-folder.jpg?alt=media&token=030c41c4-5ade-47c9-b75e-612187336b8d";
+res.set(
+     'Content-Disposition',
+     `attachment; filename=${n}`
+   );
+await request(url).pipe(res);   
+*/
+//res.redirect('/')
+  try {
+    const filename = new Date().getTime() + '-' + file.originalname;
+  const imageRef = ref(firestore, 'products/' + filename);
+//var storageRef = firestor.ref();
+//var uploadTask = ref(firestore, 'products/' + filename);
+var img = ref(firestore,"products/171yyy71174-folder.jpg" );  
+//var uploadTask = uploadBytesResumable(img , file.buffer);
+//var r =firestor.storage.ref();
+//console.log(img);
+//const fun = require("./fun.js");
+//var div = document.getElementById('div');
+  // Register three observers:
+  // 1. 'state_changed' observer, called any time the state changes
+  // 2. Error observer, called on failure
+  // 3. Completion observer, called on successful completion
+  /* 
+uploadTask.on('state_changed', 
+    (snapshot) => {
+      // Observe state change events such as progress, pause, and resume
+      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+      var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      console.log('Upload is ' + progress + '% done');
+si =progress ;
+    res.set(
+     'Content-Disposition',
+     //'attachment; filename=some_file_name.png'
+   );
+request("http://localhost:3000").pipe(res);
+//      s = progress ;
+     // res.render(__dirname + "/up2.ejs",{size:progress}); 
+     
+});*/
+//module.exports ={get(){console.log("hh")}};
+//    const snapshot = await uploadBytes(imageRef, file.buffer);
+    //const imageURL = await getDownloadURL(snapshot.ref);
+   // var pathReference =await  ref('folder.jpg');
+    //res.download(pathReference);
+//    console.log(pathReference)
+    //res.status(200).send(imageURL);
+    //res.redirect("https://firebasestorage.googleapis.com/v0/b/zinode-8e2a4.appspot.com/o/products%2F1711235971174-folder.jpg?alt=media&token=030c41c4-5ade-47c9-b75e-612187336b8d")
+//res.redirect('/')
+  } catch (error) {
+    console.log(error);
+   return ;
+  }
 });
-app.listen(3000,() => {
+
+
+
+app.listen(3000,()=>{
 console.log("listening");
 });
