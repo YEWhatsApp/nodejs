2648dd186541b51ae1ccb9d43571d993c9956f9b
WhatsApp <75611985+YEWhatsApp@users.noreply.github.com>
2024-03-28 22:24:32 +0300

    fire.js

--- fire.js
+++ fire.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get('/',(req,res)=>{
-  res.render('up2.ejs');
+  res.render('up2.ejs',{s:15,i :{get(){return "helo"}}});
 });
 app.listen(10000,()=>{
   console.log("strating");
